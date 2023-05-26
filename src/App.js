3037f38e6e012a0bff7921dc99d69108a4283cc5
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-05-26 20:28:24 -0300

    chore:remove arquivos não utilizados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 /* eslint-disable react/jsx-no-undef */
-import './App.css';
 import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Banner />
     </div>
   );
--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,7 +1,7 @@
 import './Banner.css';
 
 function Banner() {
-    return(
+    return (
         <header className='banner'>
             <img src="/imagens/banner.png" alt="banner da pagina do Organo"/>
         </header>
