f783284ef4a8d72c3c4630dd657ccde22fc0abaf
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-05-26 20:24:14 -0300

    feat: add banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css';
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="banner da pagina do Organo"/>
+        </header>
+        
+    )
+
+}
+
+export default Banner;
