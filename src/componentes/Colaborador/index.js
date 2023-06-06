34f9337c9d9828da31a74199e39863bf4e6f06cf
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-06-06 13:17:18 -0300

    excluindo colaboradores e alterando cor dos times

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,8 +1,14 @@
+import { AiFillCloseCircle } from 'react-icons/ai';
 import './Colaborador.css';
 
-const Colaborador = ({nome, imagem, cargo,corDeFundo}) => {
+const Colaborador = ({nome, imagem, cargo,corDeFundo,aoDeletar}) => {
     return(
         <div className='colaborador' key={nome}>
+            <AiFillCloseCircle 
+                size={25} 
+                className='deletar' 
+                onClick={aoDeletar} 
+            />
             <div className='cabecalho' style={{backgroundColor:corDeFundo}}>
                 <img src={imagem} alt="imagem do membro do time" />
             </div>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,21 +1,23 @@
 import Colaborador from '../Colaborador';
 import './Time.css';
+import hexToRgba from 'hex-to-rgba';
 
-const Time = (props) => {
-    const fundo = {backgroundColor:props.corSecundaria};
-    const borda = {borderColor:props.corPrimaria};
+const Time = ({colaboradores,id,nome,corPrimaria, aoDeletar,mudarCor}) => {
     return(
         //props.colaboradores.length > 0 && <section>
-        (props.colaboradores.length > 0) ? <section className="time" style={fundo}>
-            <h3 style={borda}>{props.nome}</h3>
+        (colaboradores.length > 0) ? <section className="time" style={{backgroundImage:'url(/imagens/fundo.png)',backgroundColor:hexToRgba(corPrimaria,'0.6')}}>
+            <input onChange={evento => mudarCor(evento.target.value, id)} type='color' value={corPrimaria} className='input-color'></input>
+            <h3 style={{borderColor:corPrimaria}}>{nome}</h3>
             <div className='colaboradores'>
-            {props.colaboradores.map(
+            {colaboradores.map(
                 colaborador => 
                     <Colaborador
-                        corDeFundo={props.corPrimaria} 
+                        key={colaborador.id}
+                        corDeFundo={corPrimaria} 
                         nome={colaborador.nome} 
                         cargo={colaborador.cargo}
                         imagem={colaborador.imagem}
+                        aoDeletar={() => aoDeletar(colaborador.id)}
                         />
                 )
             }
